cb722d46e3e193ff8d3fce2496287d9b7809b472
imrookhan44 <aqib@surfmobil.no>
2023-12-29 21:56:33 +0500

    fixes

--- src/service/Baseurl.js
+++ src/service/Baseurl.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:4000" });
+// const API = axios.create({ baseURL: "http://localhost:4000" });
+const API = axios.create({ baseURL: "https://actagonbackend.onrender.com" });
 
 export default API;
